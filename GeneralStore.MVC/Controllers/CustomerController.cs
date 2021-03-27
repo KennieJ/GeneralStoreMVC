9103b6d6d1cf8f7c9b93427bbf8df1004b2a2d07
KennieJ <kennie.jfive@gmail.com>
2021-03-27 19:58:36 -0400

    Establish Customer home page view

---
+++ GeneralStore.MVC/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private CustomerDbContext _db = new CustomerDbContext();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(_db.Customers.ToList());
+        }
+    }
+}
